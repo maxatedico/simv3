2f69e60047d59d8c72d2401607c42646c5ef9455
Max <max.rosoff@edicogenome.com>
2017-07-17 16:31:59 -0700

    new cleaner file

--- cleaner.py
+++ cleaner.py
@@ -18,7 +18,7 @@ def clean_up(input_error):
         #logger.info("Clean-Up Command: {}".format(cmd))
         #logger.info("ReAlign Command: {}".format(cmd_2))
         subprocess.check_output(cmd, shell=True)
-        subprocess.check_output(cmd_2, shell=True)
+        #subprocess.check_output(cmd_2, shell=True)
 
     except Exception() as e:
         #logging.error('Error message %s' % e)
