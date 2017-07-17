ab3cb9a18a3c9195ed8f8886511bb873e01e12e3
Max <max.rosoff@edicogenome.com>
2017-07-17 16:19:39 -0700

    new cleaner file

--- cleaner.py
+++ cleaner.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-import logging
+#import logging
 import subprocess
 
-logger = logging.getLogger(__name__)
+#logger = logging.getLogger(__name__)
 
 
 def clean_up(input_error):
@@ -12,13 +12,13 @@ def clean_up(input_error):
         cmd = path + "clean_read_info.pl < " + input_error + " > " + path + "clean.read.info"
         cmd_2 = path + "xform_read.info.pl < " + path + "clean.read.info > " + path + "read.info.xform"
 
-        logger.info("Clean-Up Command: {}".format(cmd))
-        logger.info("ReAlign Command: {}".format(cmd_2))
+        #logger.info("Clean-Up Command: {}".format(cmd))
+        #logger.info("ReAlign Command: {}".format(cmd_2))
         subprocess.check_output(cmd, shell=True)
         subprocess.check_output(cmd_2, shell=True)
 
     except Exception() as e:
-        logging.error('Error message %s' % e)
+        #logging.error('Error message %s' % e)
         raise
 
 clean_up("/mnt/archive/sim_data/pirs_simulator/vlrd_chr1_normalNoise_varRate0.002_indelErrors1_errorRate1_varRate0.002")
